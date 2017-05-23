ded17c6242ca02ee06dc5c568714f42f77799017
ummumm <phu-mi-phat@hotmail.com>
2017-05-24 00:12:41 +0700

    Third copy of all files

--- app/build/generated/source/r/debug/com/example/richy/newrecipenest/R.java
+++ app/build/generated/source/r/debug/com/example/richy/newrecipenest/R.java
@@ -3097,7 +3097,7 @@ containing a value of this type.
         public static final int avd_show_password_1=0x7f020070;
         public static final int avd_show_password_2=0x7f020071;
         public static final int avd_show_password_3=0x7f020072;
-        public static final int background=0x7f020055;
+        public static final int background1=0x7f020055;
         public static final int design_bottom_navigation_item_background=0x7f020056;
         public static final int design_fab_background=0x7f020057;
         public static final int design_ic_visibility=0x7f020058;
@@ -3400,9 +3400,11 @@ containing a value of this type.
     }
     public static final class mipmap {
         public static final int addrecipe=0x7f030000;
-        public static final int ic_launcher=0x7f030001;
-        public static final int ic_launcher_round=0x7f030002;
-        public static final int seerecipes=0x7f030003;
+        public static final int email=0x7f030001;
+        public static final int ic_launcher=0x7f030002;
+        public static final int ic_launcher_round=0x7f030003;
+        public static final int myrecipe=0x7f030004;
+        public static final int seerecipes=0x7f030005;
     }
     public static final class string {
         public static final int abc_action_bar_home_description=0x7f070000;
